1b73261d97f7f419a65fa92b576909fa556b7d2a
LazyIce <1542752218@qq.com>
2019-12-04 01:11:32 -0500

    refactor the styles

--- frontend/client/src/components/Spinner.js
+++ frontend/client/src/components/Spinner.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import Loader from 'react-loader-spinner'
 
 export default () => 
-  <div className='rolling'>
-    <div className='spinner fadein'>
-      <FontAwesomeIcon icon={faSpinner} size='5x' color='#268cf1' />
-    </div>
-  </div>
+  <Loader
+    type="ThreeDots"
+    color="#476e9e"
+    height={160}
+    width={160}
+  />
